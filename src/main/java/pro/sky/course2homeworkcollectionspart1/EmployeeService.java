8352e42108bdc178da8795933aa3cbe496b5e7f2
elena.denoeva <denoeva.elena@skyeng.ru>
2023-05-04 20:49:27 +0300

    Добавлен класс Employee из Курсовой

---
+++ src/main/java/pro/sky/course2homeworkcollectionspart1/EmployeeService.java
@@ -0,0 +1,5 @@
+package pro.sky.course2homeworkcollectionspart1;
+
+public interface EmployeeService {
+
+}
---
+++ src/main/java/pro/sky/course2homeworkcollectionspart1/EmployeeServiceImpl.java
@@ -0,0 +1,8 @@
+package pro.sky.course2homeworkcollectionspart1;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmployeeServiceImpl {
+
+}
