e66cf857dcb5ed6d0652833e317668b58552ce55
elena.denoeva <denoeva.elena@skyeng.ru>
2023-05-04 20:33:45 +0300

    Initial commit

---
+++ src/main/java/pro/sky/course2homeworkcollectionspart1/Course2HomeworkCollectionsPart1Application.java
@@ -0,0 +1,13 @@
+package pro.sky.course2homeworkcollectionspart1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Course2HomeworkCollectionsPart1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Course2HomeworkCollectionsPart1Application.class, args);
+	}
+
+}
---
+++ src/test/java/pro/sky/course2homeworkcollectionspart1/Course2HomeworkCollectionsPart1ApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.course2homeworkcollectionspart1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Course2HomeworkCollectionsPart1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
