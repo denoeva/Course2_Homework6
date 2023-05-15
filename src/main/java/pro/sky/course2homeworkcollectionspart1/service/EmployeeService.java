1b2bcce152763a53218669d329a634f001afaabc
elena.denoeva <denoeva.elena@skyeng.ru>
2023-05-15 21:54:23 +0300

    Домашнаяя работа - Коллекции: многообразие реализаций

--- src/main/java/pro/sky/course2homeworkcollectionspart1/service/EmployeeService.java
+++ src/main/java/pro/sky/course2homeworkcollectionspart1/service/EmployeeService.java
@@ -6,41 +6,45 @@ import pro.sky.course2homeworkcollectionspart1.exceptions.EmployeeAlreadyAddedEx
 import pro.sky.course2homeworkcollectionspart1.exceptions.EmployeeNotFoundException;
 import pro.sky.course2homeworkcollectionspart1.exceptions.EmployeeStorageIsFullException;
 
-import java.util.ArrayList;
-import java.util.Collection;
+import java.util.*;
 
 @Service
 public class EmployeeService {
     private static final int SIZE_LIMIT = 5;
-    private final Collection<Employee> employees = new ArrayList(SIZE_LIMIT);
+    private final Map<String, Employee> employees = new HashMap<>(SIZE_LIMIT);
 
     public Collection<Employee> getAll() {
-        return employees;
+        return employees.values();
     }
 
     public Employee add(Employee employee) {
         if (employees.size() >= SIZE_LIMIT) {
             throw new EmployeeStorageIsFullException();
         }
-        if (employees.contains(employee)) {
+        if (employees.containsKey(createEmployeeKey(employee))) {
             throw new EmployeeAlreadyAddedException();
         }
-        employees.add(employee);
+        employees.put(createEmployeeKey(employee), employee);
         return employee;
     }
 
     public Employee find(String firstName, String lastName) {
-        for (Employee employee : employees) {
-            if (employee.getFirstName().equals(firstName) && employee.getLastName().equals(lastName)) {
-                return employee;
-            }
+        Employee employee = employees.get(createEmployeeKey(firstName, lastName));
+        if (employee == null) {
+            throw new EmployeeNotFoundException();
         }
-        throw new EmployeeNotFoundException();
+        return employee;
     }
 
     public Employee remove(String firstName, String lastName) {
-        Employee employee = find(firstName, lastName);
-        employees.remove(employee);
-        return employee;
+        return employees.remove(createEmployeeKey(firstName, lastName));
+    }
+
+    public static String createEmployeeKey(Employee employee) {
+            return createEmployeeKey(employee.getFirstName(), employee.getLastName());
+    }
+
+    private static String createEmployeeKey(String firstName, String lastName) {
+        return (firstName + lastName).toLowerCase();
     }
 }
